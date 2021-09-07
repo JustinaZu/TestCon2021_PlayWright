59aa1b016b80628628137bfdeca438c922e26f32
Justina Žuravskienė <90178770+JustinaZu@users.noreply.github.com>
2021-09-07 11:54:00 +0300

    checking logo
    first test

--- tests/duck.spec.js
+++ tests/duck.spec.js
@@ -5,3 +5,14 @@ test('basic test', async ({ page }) => {
   const title = page.locator('.navbar__inner .navbar__title');
   await expect(title).toHaveText('Playwright');
 });
+
+test('duck go checking logo', async ({ page }) => {
+  await page.goto('https://duckduckgo.com/');
+  const logo = page.locator('#logo_homepage_link');
+  //await expect(logo.isVisible()).toBe();
+  await expect(logo).toBeVisible(true);
+
+
+
+
+});
